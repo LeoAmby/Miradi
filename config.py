e5e81d0168168c54eaa3e88f198c66f249efb92b
MungaiKeren <wambukeren@gmail.com>
2019-09-25 11:48:39 +0300

    this commit edits the configurations file

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    pass
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    pass
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
